52325cb1665322575d344dad2e640eac03f0ed32
NazarKutoviy <nazarkutoviy@gmail.com>
2024-08-12 21:07:42 +0300

    add

---
+++ index.js
@@ -0,0 +1,25 @@
+let Age = 12;
+console.log(Age);
+
+const userName = "Nazar";
+console.log(userName);
+
+let isStudent = true;
+console.log(isStudent);
+
+let myString = "Хто не жив посеред бурі, той ціни не знає силі.";
+console.log(myString);
+
+let myNumber = 22;
+myNumber = myNumber + 10;
+console.log(myNumber);
+
+let myNull = null;
+console.log(myNull);
+
+let userFirstName = prompt("what your name");
+console.log(userFirstName);
+let message = (`hi ${userFirstName}`)
+alert(message);
+// 8 cancelled
+// 9 cancelled
